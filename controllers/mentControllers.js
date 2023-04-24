26c1176a4841707d14e4425504c61d3adef92743
can-ishk <kchathley26@gmail.com>
2023-04-24 15:12:40 +0530

    search implemented

--- controllers/mentControllers.js
+++ controllers/mentControllers.js
@@ -115,27 +115,30 @@ export const getMents = async (req, res) => {
 
     if (!sortBy) sortBy = "-createdAt";
     if (!page) page = 1;
-
-    let ments = await Ment.find()
-      .populate("author", "-password")
-      .sort(sortBy)
-      .lean();
+    let ments;
+    if (search) {
+      ments = await Ment.find({$text: {$search: search}})
+        .populate("author", "-password")
+        .sort(sortBy)
+        .lean();
+    }
+    else{
+      ments = await Ment.find()
+        .populate("author", "-password")
+        .sort(sortBy)
+        .lean();
+    }
 
     if (author) {
       ments = ments.filter((ment) => ment.author.username == author);
     }
 
-    if (search) {
-      ments = ments.filter((ment) =>
-        ment.title.toLowerCase().includes(search.toLowerCase())
-      );
-    }
 
     const count = ments.length;
 
     ments = paginate(ments, 10, page);
 
-    return res.json({ data: ments, count });
+    return res.json({ data: ments, count, search: search? true: false });
   } catch (err) {
     return res.status(400).json({ error: err.message });
   }
--- models/Ment.js
+++ models/Ment.js
@@ -1,38 +1,39 @@
 import { Schema, Types, model } from "mongoose";
 //TODO: add profanity filter
 const MentSchema = new Schema(
-    {
-      author: {
-        type: Types.ObjectId,
-        ref: "user",
-        required: true,
-      },
-      title: {
-        type: String,
-        required: true,
-        maxLength: [80, "<40"],
-      },
-      content: {
-        type: String,
-        required: true,
-        maxLength: [2000, "<500"],
-      },
-      edited: {
-        type: Boolean,
-        default: false,
-      },
-      projectTags:{
-        type: [String],
-      },
-      tags: {
-        type: [String],
-      },
-      type:{
-        type: String,
-      }
+  {
+    author: {
+      type: Types.ObjectId,
+      ref: "user",
+      required: true,
     },
-    { timestamps: true }
-  );
+    title: {
+      type: String,
+      required: true,
+      maxLength: [80, "<40"],
+    },
+    content: {
+      type: String,
+      required: true,
+      maxLength: [2000, "<500"],
+    },
+    edited: {
+      type: Boolean,
+      default: false,
+    },
+    projectTags: {
+      type: [String],
+    },
+    tags: {
+      type: [String],
+    },
+    type: {
+      type: String,
+    }
+  },
+  { timestamps: true }
+);
 
+MentSchema.index({ title: "text", tags: "text", content: "text", projectTags: "text", type: "text" })
 
 export default model("ment", MentSchema);
